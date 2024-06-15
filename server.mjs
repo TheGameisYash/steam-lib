20c401239d040c037df47ba53066d2d77dc5a634
TheGameisYash <yash237yash@gmail.com>
2024-06-15 15:38:11 +0530

    Add environment variables and secure API keys

--- server.mjs
+++ server.mjs
@@ -3,19 +3,22 @@ import path from 'path';
 import fetch from 'node-fetch';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Proxy route to fetch Steam API data
 app.get('/api/games', async (req, res) => {
-  const apiKey = '2F301F075DF529D0929093D7E3E934CB';
-  const steamId = '76561198956603444';
+  const apiKey = process.env.STEAM_API_KEY;
+  const steamId = process.env.STEAM_USER_ID;
   const apiUrl = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${apiKey}&steamid=${steamId}&include_appinfo=true&include_played_free_games=true`;
 
   try {
